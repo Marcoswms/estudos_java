7fd9e9b540c8401f7b335bf40d8790856584d7c2
Marcos Willian <marcoswms@gmail.com>
2025-02-25 03:03:33 -0300

    Add teórico sobre Listas

---
+++ vetores_estudo/src/application/Listas.java
@@ -0,0 +1,24 @@
+package application;
+
+public class Listas {
+    public static void main(String[] args) {
+        /*Listas
+        * Apenas dados do mesmo tipo;
+        * Ordenada (Elemento acessado por meio de posição);
+        * Inicia vazia e seus elementos são alocados sob demanda;
+        * Cada elemento ocupa um nó (ou nodo) da lista (nodo é a sequência que a lista percorrerá até algo apontar
+          seu término);
+
+        * Tipo (interface): List (Tipo interface não poderá ser instanciada)
+        * No caso acima, 'List' pode ser instanciada com Classes que implementam essa interface: ArrayList, LinkedList..
+
+        * Vantagens:
+        * Tamanho variável - (Um Vetor é necessário informar quantas posições possuirá,a Lista não tem essa necessidade,
+          ela poderá ser preenchida de acordo com sua necessidade);
+        * Facilidade para se realizar inserções e deleções;
+
+        * Desvantagens:
+        * Acesso sequencial aos elementos - (Para acessar a posição 10, terá de percorrer da 1ª até a 10ª posição porém
+          existe a possibilidade de 'otimizar' esse acesso com o ArrayList (um hibrido entre List e Vetor)); */
+    }
+}
