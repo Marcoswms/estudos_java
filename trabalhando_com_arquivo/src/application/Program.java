23b252c2b1f435b828576114ead66cfa31b3e274
Marcos Willian <marcoswms@gmail.com>
2026-06-21 20:23:34 -0300

    feat: adiciona aula de manipulação de arquivos

---
+++ trabalhando_com_arquivo/src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        File file = new File("C:\\Users\\Marcos\\Documents\\IntelliJ_Projetos\\Exercicios_Java\\trabalhando_com_arquivo\\src\\files\\test.txt");
+
+        Scanner scan = null;
+        try {
+            scan = new Scanner(file);
+            while (scan.hasNextLine()) {
+                System.out.println(scan.nextLine());
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        finally {
+            if (scan != null) {
+                scan.close();
+            }
+        }
+    }
+}
