46294240df6f191011ae994d2986016c8b661c3f
Marcos Willian <marcoswms@gmail.com>
2026-06-22 22:32:49 -0300

    feat: adiciona exemplo para try with resource, simplifica a forma de instanciar as streams

---
+++ trabalhando_com_arquivo/src/application/ExemploTryWithResource.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ExemploTryWithResource {
+    public static void main(String[] args) {
+
+        String path = "C:\\Users\\Marcos\\Documents\\IntelliJ_Projetos\\Exercicios_Java\\trabalhando_com_arquivo\\src\\files\\test.txt";
+
+        //Instanciar as 'streams' dentro do try(aqui) deixa o código mais legível e prático.
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
